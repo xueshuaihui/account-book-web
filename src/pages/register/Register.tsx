374cac5e2d5784d14152b2ac3e9738e0a3980f21
xueshuaihui <534684120@qq.com>
2024-08-14 11:57:09 +0000

    完善结构

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,8 +1,24 @@
+import { Form, Input } from 'antd-mobile'
+import styles from './Register.module.scss';
 
 const Register = () => {
   return (
     <div>
-      <h1>Register</h1>
+      <Form layout='horizontal'>
+      <Form.Item label='手机号' name='username'>
+            <Input placeholder='手机号' clearable />
+          </Form.Item>
+          <Form.Item
+            label='短信验证码'
+            extra={
+              <div className={styles.extraPart}>
+                <a>发送验证码</a>
+              </div>
+            }
+          >
+            <Input placeholder='请输入验证码' clearable />
+          </Form.Item>
+        </Form>
     </div>
   )
 }
